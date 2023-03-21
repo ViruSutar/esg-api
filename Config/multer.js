d5c6c58b56e407b3723633b3c1aae02d5a69dea9
ViruSutar <72197422+ViruSutar@users.noreply.github.com>
2023-03-21 14:48:26 +0530

    []

--- Config/multer.js
+++ Config/multer.js
@@ -2,7 +2,8 @@ const multer = require('multer');
 const multerS3 = require('multer-s3');
 const dotenv = require('dotenv')
 dotenv.config({ path: './dev.env' })
-const AWS = require('@aws-sdk/client-s3');
+// const AWS = require('@aws-sdk/client-s3');
+const AWS = require('aws-sdk')
 
   const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
